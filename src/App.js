bbeadd348eff0999751e0819c0fdf83c8417ccf5
Lailz <lailaabdulraheem@gmail.com>
2020-05-27 12:48:12 +0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ import {
   GlobalStyle,
   ShopImage,
   ThemeButton,
-  Title
+  Title,
 } from "./styles";
 
 const theme = {
   light: {
     mainColor: "#242424", // main font color
     backgroundColor: "#fefafb", // main background color
-    pink: "#ff85a2"
+    pink: "#ff85a2",
   },
   dark: {
     mainColor: "#fefafb", // main font color
     backgroundColor: "#242424", // main background color
-    pink: "#ff85a2"
-  }
+    pink: "#ff85a2",
+  },
 };
 
 function App() {
@@ -38,14 +38,12 @@ function App() {
       <ThemeButton onClick={toggleTheme}>
         {theme === "light" ? "Dark" : "Light"} Mode
       </ThemeButton>
-      <>
-        <Title>Cookies and Beyond</Title>
-        <Description>Where cookie maniacs gather</Description>
-        <ShopImage
-          alt="cookie shop"
-          src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
-        />
-      </>
+      <Title>Cookies and Beyond</Title>
+      <Description>Where cookie maniacs gather</Description>
+      <ShopImage
+        alt="cookie shop"
+        src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
+      />
       <CookieList />
     </ThemeProvider>
   );
