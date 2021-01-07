69eaced02c81580403ae6b440918569bd64ca58c
Lailz <lailaabdulraheem@gmail.com>
2021-01-07 10:40:54 +0300

    add button and fix theme object

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
+// Styling
+import { GlobalStyle, ThemeButton } from "./styles";
+
 // Components
 import CookieList from "./components/ProductList";
-// Styling
-import { GlobalStyle } from "./styles";
 import Home from "./components/Home";
 import { ThemeProvider } from "styled-components";
 
 const theme = {
-  mainColor: "#242424", // main font color
-  backgroundColor: "#fefafb", // main background color
-  pink: "#ff85a2",
+  light: {
+    mainColor: "#242424", // main font color
+    backgroundColor: "#fefafb", // main background color
+    pink: "#ff85a2",
+    red: "#ff3232",
+  },
+  dark: {
+    mainColor: "#fefafb", // main font color
+    backgroundColor: "#242424", // main background color
+    pink: "#ff85a2",
+    red: "#ff3232",
+  },
 };
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme.light}>
       <GlobalStyle />
+      <ThemeButton onClick={() => alert("I do nothing..")}>
+        Dark Theme
+      </ThemeButton>
       <Home />
       <CookieList />
     </ThemeProvider>
--- src/styles.js
+++ src/styles.js
@@ -1,22 +1,5 @@
 import styled, { createGlobalStyle } from "styled-components";
 
-export const ProductWrapper = styled.div`
-  margin: 20px;
-
-  img {
-    width: 200px;
-    height: 200px;
-  }
-
-  p {
-    text-align: center;
-
-    &.product-price {
-      color: ${(props) => props.theme.pink};
-    }
-  }
-`;
-
 export const Description = styled.h4`
   text-align: center;
 `;
@@ -34,8 +17,21 @@ export const ListWrapper = styled.div`
   display: flex;
 `;
 
-export const Title = styled.h1`
-  text-align: center;
+export const ProductWrapper = styled.div`
+  margin: 20px;
+
+  img {
+    width: 200px;
+    height: 200px;
+  }
+
+  p {
+    text-align: center;
+
+    &.product-price {
+      color: ${(props) => props.theme.pink};
+    }
+  }
 `;
 
 export const ShopImage = styled.img`
@@ -43,4 +39,17 @@ export const ShopImage = styled.img`
   margin-left: auto;
   margin-right: auto;
   width: 50%;
+`;
+
+export const ThemeButton = styled.button`
+  font-size: 1em;
+  margin: 1.25em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  background-color: ${(props) => props.theme.mainColor};
+  color: ${(props) => props.theme.backgroundColor};
+`;
+
+export const Title = styled.h1`
+  text-align: center;
 `;
